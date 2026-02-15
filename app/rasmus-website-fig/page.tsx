5f5359e7119e6214ffa2fa2838a4a321f6a811c5
Rasmus <rasmus.diessel@gmx.de>
2026-02-15 19:04:11 +0100

    base website layout

--- app/rasmus-website-fig/page.tsx
+++ app/rasmus-website-fig/page.tsx
@@ -1,106 +1,28 @@
 import MyMarkdown from "./mymarkdown.mdx";
  
-
-
-
-
 export default async function Contact() {
-	// return (
-	// 	<div className="flex w-300 h-300 bg-green-700 justify-center items-center p-6">
-	// 		<p className="text-white w-100 text-center justify-center text-5xl font-normal font-['Inter']">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsam rem sapiente vero, dignissimos deleniti libero beatae? Laboriosam a, voluptate quia voluptas atque quod, ea ratione molestiae neque eum quisquam.</p>
-	// 	</div>
-	// );
-
-	// return (
-	// 		<div className="flex justify-center items-center w-[753px] h-96 relative bg-green-700 overflow-hidden">
-	// 			<div className="absolute text-center justify-center text-white text-2xl font-normal font-['Inter']">Test Test</div>
-	// 		</div>
-	// );
-
 	return (
-		<div className="w-[1620px] relative inline-flex flex-col justify-start items-start">
-			<div className="w-[1620px] h-[890px] bg-stone-900" />
-			<div className="w-[1620px] h-7 relative overflow-hidden">
-				<div className="w-[1620px] h-7 left-0 top-0 absolute bg-neutral-800" />
-				<div className="w-14 h-3.5 left-[17.79px] top-[5.93px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">master *</div>
-				<div className="w-24 h-3.5 left-[88.95px] top-[5.93px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">rasmus-website</div>
-				<div className="w-32 h-3.5 left-[222.37px] top-[5.93px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">0 errors 4 warnings</div>
-				<div className="w-16 h-3.5 left-[1389.84px] top-[5.93px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">Ln 1, Col 1</div>
-				<div className="w-10 h-3.5 left-[1467.67px] top-[5.93px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">UTF-8</div>
-				<div className="w-24 h-3.5 left-[1523.27px] top-[5.93px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">TypeScript JSX</div>
+		<div className="flex flex-col w-full h-screen">
+			<div className="flex flex-row w-full h-15 shrink-0 bg-neutral-800 items-center gap-6 px-4 border-2 border-solid border-amber-50">
+				<p className="text-white text-3xl font-normal font-['Inter']">Home</p>
+				<p className="text-white text-3xl font-normal font-['Inter']">Projects</p>
+				<p className="text-white text-3xl font-normal font-['Inter']">Contact</p>
 			</div>
-			<div className="w-[1255px] h-[815.49px] left-[365px] top-[70.91px] absolute overflow-hidden">
-				<div className="w-[1255px] h-[1344.21px] left-0 top-0 absolute bg-stone-900" />
-				<div className="w-12 h-4 left-[15.03px] top-[8.43px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">import</div>
-				<div className="w-9 h-4 left-[71.39px] top-[8.43px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> type </div>
-				<div className="w-16 h-4 left-[121.49px] top-[8.43px] absolute justify-start text-emerald-400 text-xs font-normal font-['Inter']">Metadata</div>
-				<div className="w-9 h-4 left-[196.64px] top-[8.43px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> from </div>
-				<div className="w-11 h-4 left-[246.74px] top-[8.43px] absolute justify-start text-red-400 text-xs font-normal font-['Inter']">&quot;next&quot;</div>
-				<div className="w-12 h-4 left-[15.03px] top-[29.50px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">import</div>
-				<div className="w-0 h-4 left-[71.39px] top-[29.50px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> </div>
-				<div className="w-24 h-4 left-[96.44px] top-[29.50px] absolute justify-start text-red-400 text-xs font-normal font-['Inter']">&quot;./globals.css&quot;</div>
-				<div className="w-2.5 h-4 left-[15.03px] top-[50.57px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">&amp;</div>
-				<div className="w-12 h-4 left-[15.03px] top-[71.65px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">export</div>
-				<div className="w-0 h-4 left-[83.92px] top-[71.65px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> </div>
-				<div className="w-10 h-4 left-[96.44px] top-[71.65px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">const</div>
-				<div className="w-20 h-4 left-[146.54px] top-[71.65px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> metadata: </div>
-				<div className="w-16 h-4 left-[234.22px] top-[71.65px] absolute justify-start text-emerald-400 text-xs font-normal font-['Inter']">Metadata</div>
-				<div className="w-12 h-4 left-[309.37px] top-[71.65px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> = &#123; &amp; &#125;</div>
-				<div className="w-12 h-4 left-[15.03px] top-[92.72px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">export</div>
-				<div className="w-0 h-4 left-[83.92px] top-[92.72px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> </div>
-				<div className="w-12 h-4 left-[96.44px] top-[92.72px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">default</div>
-				<div className="w-0 h-4 left-[177.85px] top-[92.72px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> </div>
-				<div className="w-14 h-4 left-[190.38px] top-[92.72px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">function</div>
-				<div className="w-28 h-4 left-[271.79px] top-[92.72px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> RootLayout(&amp;) &#123;</div>
-				<div className="w-11 h-4 left-[15.03px] top-[113.79px] absolute justify-start text-zinc-400 text-xs font-normal font-['Inter']">return</div>
-				<div className="w-2.5 h-4 left-[96.44px] top-[113.79px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> (</div>
-				<div className="w-0 h-4 left-[15.03px] top-[134.86px] absolute justify-start"></div>
-				<div className="w-8 h-4 left-[33.82px] top-[134.86px] absolute justify-start text-blue-300 text-xs font-normal font-['Inter']">html</div>
-				<div className="w-6 h-4 left-[83.92px] top-[134.86px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> &amp;&gt;</div>
-				<div className="w-0 h-4 left-[15.03px] top-[155.93px] absolute justify-start"></div>
-				<div className="w-9 h-4 left-[40.08px] top-[155.93px] absolute justify-start text-blue-300 text-xs font-normal font-['Inter']">body</div>
-				<div className="w-6 h-4 left-[90.18px] top-[155.93px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']"> &amp;&gt;</div>
-				<div className="w-0 h-4 left-[15.03px] top-[177.01px] absolute justify-start"></div>
-				<div className="w-4 h-4 left-[15.03px] top-[198.08px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">&lt;/</div>
-				<div className="w-9 h-4 left-[46.34px] top-[198.08px] absolute justify-start text-blue-300 text-xs font-normal font-['Inter']">body</div>
-				<div className="w-2.5 h-4 left-[96.44px] top-[198.08px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">&gt;</div>
-				<div className="w-4 h-4 left-[15.03px] top-[219.15px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">&lt;/</div>
-				<div className="w-8 h-4 left-[40.08px] top-[219.15px] absolute justify-start text-blue-300 text-xs font-normal font-['Inter']">html</div>
-				<div className="w-2.5 h-4 left-[83.92px] top-[219.15px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">&gt;</div>
+			<div className="flex flex-1 min-h-0 flex-row w-full bg-stone-500">
+				<div className="flex flex-col w-120 shrink-0 bg-blue-500">
+					<p className="text-white text-3xl font-normal font-['Inter']">mobileminigames.cs</p>
+					<p className="text-white text-3xl font-normal font-['Inter']">hotreload.cs</p>
+					<p className="text-white text-3xl font-normal font-['Inter']">athenegpt.go</p>
+				</div>
+				<div className="min-h-0 flex-1 overflow-auto bg-stone-500">
+					<div className="prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white">
+						<MyMarkdown/>
+					</div>
+				</div>
 			</div>
-			<div className="w-[1620px] h-9 left-0 top-0 absolute overflow-hidden">
-				<div className="w-[1620px] h-9 left-0 top-0 absolute bg-zinc-800" />
-				<div className="w-6 h-3.5 left-[23.14px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">File</div>
-				<div className="w-6 h-3.5 left-[63.64px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">Edit</div>
-				<div className="w-16 h-3.5 left-[109.93px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">Selection</div>
-				<div className="w-8 h-3.5 left-[190.93px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">View</div>
-				<div className="w-5 h-3.5 left-[243px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">Go</div>
-				<div className="w-6 h-3.5 left-[289.29px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">Run</div>
-				<div className="w-14 h-3.5 left-[335.57px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">Terminal</div>
-				<div className="w-8 h-3.5 left-[410.79px] top-[9.78px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">Help</div>
-				<div className="w-60 h-3.5 left-[636.43px] top-[9.78px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">layout.tsx - rasmus-website - Cursor</div>
-			</div>
-			<div className="w-14 h-[852.73px] left-0 top-[36.09px] absolute overflow-hidden">
-				<div className="w-14 h-[852.73px] left-0 top-0 absolute bg-zinc-800" />
-				<div className="w-7 h-6 left-[13.43px] top-[11.21px] absolute bg-zinc-700" />
-				<div className="w-7 h-6 left-[13.43px] top-[43.81px] absolute bg-zinc-700" />
-			</div>
-			<div className="w-80 h-[853.45px] left-[53.37px] top-[35.69px] absolute overflow-hidden">
-				<div className="w-80 h-[853.45px] left-0 top-0 absolute bg-neutral-800" />
-				<div className="w-32 h-3.5 left-[18.90px] top-[17.76px] absolute justify-start text-neutral-300 text-xs font-bold font-['Inter']">RASMUS-WEBSITE</div>
-				<div className="w-16 h-3.5 left-[41.14px] top-[165.76px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">favicon.ico</div>
-				<div className="w-16 h-3.5 left-[41.14px] top-[185.49px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">globals.css</div>
-				<div className="w-80 h-5 left-0 top-[210.16px] absolute bg-sky-900" />
-				<div className="w-16 h-3.5 left-[41.14px] top-[213.12px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">layout.tsx</div>
-				<div className="w-14 h-3.5 left-[41.14px] top-[234.82px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">page.tsx</div>
-			</div>
-			<div className="w-64 h-9 left-[364.69px] top-[35.69px] absolute overflow-hidden">
-				<div className="w-32 h-9 left-0 top-0 absolute bg-zinc-800" />
-				<div className="w-16 h-4 left-[13.34px] top-[10.20px] absolute justify-start text-zinc-500 text-xs font-normal font-['Inter']">Untitled.fig</div>
-				<div className="w-32 h-9 left-[133.42px] top-0 absolute bg-stone-900" />
-				<div className="w-32 h-0 left-[133.42px] top-[33.65px] absolute bg-black outline outline-2 outline-offset-[-1px] outline-sky-600" />
-				<div className="w-16 h-4 left-[146.77px] top-[10.20px] absolute justify-start text-neutral-300 text-xs font-normal font-['Inter']">layout.tsx</div>
-				<div className="w-0 h-4 left-[252.39px] top-[10.20px] absolute justify-start"></div>
+			<div className="w-full h-10 shrink-0 bg-neutral-800 flex flex-row items-center gap-6 px-4 border-2 border-solid border-amber-50">
+				<p className="text-white text-2xl font-normal font-['Inter']">Impressum</p>
+				<p className="text-white text-2xl font-normal font-['Inter']">Privacy Policy</p>
 			</div>
 		</div>
 	);

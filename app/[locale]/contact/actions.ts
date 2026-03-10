c6f22a5f0787da52fb81aebd18efe1db650948f0
Rasmus <rasmus.diessel@gmx.de>
2026-03-10 16:38:34 +0100

    added zod package and test handler

---
+++ app/[locale]/contact/actions.ts
@@ -0,0 +1,14 @@
+'use server'
+
+import { z } from "zod"
+
+const schema = z.object({
+  username: z.string().min(3).max(20),
+})
+
+export default async function handleSubmit(formdata: FormData) {
+  const result = schema.safeParse({
+    username: formdata.get("username")
+  })
+  console.log(result.data?.username)
+}

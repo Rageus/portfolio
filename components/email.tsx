eac756f8c6d9cf573d627f358d3f241c87af0fdb
Rasmus <rasmus.diessel@gmx.de>
2026-03-12 00:33:57 +0100

    removed unneeded import

--- components/email.tsx
+++ components/email.tsx
@@ -1,10 +1,8 @@
-import { Heading } from '@react-email/heading'
 import { Html } from '@react-email/html'
 
 export default function Email({ firstName, message }: { firstName: string; message: string }) {
   return (
     <Html>
-      <Heading>Mail from rasmus-diessel.com. Sender Name: {firstName}</Heading>
       {message}
     </Html>
   );

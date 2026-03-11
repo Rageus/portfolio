1881b7d77e7eaaa4b0256691ec76ff4a8723e89d
Rasmus <rasmus.diessel@gmx.de>
2026-03-12 00:33:38 +0100

    potentially fixed sending mails in cloudflare

--- components/email.tsx
+++ components/email.tsx
@@ -1,10 +1,11 @@
-import { Button, Html, Head, Body, Heading } from "@react-email/components";
+import { Heading } from '@react-email/heading'
+import { Html } from '@react-email/html'
 
 export default function Email({ firstName, message }: { firstName: string; message: string }) {
   return (
     <Html>
       <Heading>Mail from rasmus-diessel.com. Sender Name: {firstName}</Heading>
-      <Body>{message}</Body>
+      {message}
     </Html>
   );
 }

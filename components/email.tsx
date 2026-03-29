b0d78ba128941fae38036a385791a77ba9eb0165
Rasmus <rasmus.diessel@gmx.de>
2026-03-29 19:50:45 +0200

    added sendername

--- components/email.tsx
+++ components/email.tsx
@@ -2,6 +2,7 @@
 export default function Email({ firstName, message }: { firstName: string; message: string }) {
   return (
     <html>
+      sender name: {firstName}
       {message}
     </html>
   );

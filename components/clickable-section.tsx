5d09c5a4a188db58b983bb122f0069a9ef45f542
Rasmus <rasmus.diessel@gmx.de>
2026-04-25 16:39:07 +0200

    added arrow in selectable sections

--- components/clickable-section.tsx
+++ components/clickable-section.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ArrowUpRight } from "lucide-react";
 import { Link } from "@/i18n/navigation";
 
 export default function ClickableSection({
@@ -9,11 +10,15 @@ export default function ClickableSection({
   href: string;
   children: React.ReactNode;
 }) {
-  const classes =
-    "block w-full cursor-pointer transition-colors hover:bg-file-hover no-underline hover:no-underline text-inherit hover:text-inherit visited:text-inherit";
-
   return (
-    <Link href={href} className={classes}>
+    <Link
+      href={href}
+      className="group relative block w-full cursor-pointer transition-colors hover:bg-file-hover no-underline hover:no-underline text-inherit hover:text-inherit visited:text-inherit"
+    >
+      <ArrowUpRight
+        className="absolute top-2 right-2 text-britty-font transition-all duration-200 group-hover:text-britty-highlight group-hover:scale-125"
+        size={16}
+      />
       {children}
     </Link>
   );
